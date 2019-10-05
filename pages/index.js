efa9edb9ad11c373a63d232654468804e9aa7cd0
zhanning.bzn <zhanning.bzn@antfin.com>
2019-10-05 16:41:40 +0800

    添加是否渲染组件的标识值

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,18 @@ export default function() {
   let graph = null
 
   // 边tooltip坐标
+  const [showEdgeTooltip, setShowEdgeTooltip] = useState(false)
   const [edgeTooltipX, setEdgeTooltipX] = useState(0)
-  const [edgeTooltipY, setEdgeTooltipY] = useState(-300)
+  const [edgeTooltipY, setEdgeTooltipY] = useState(0)
 
   // 节点tooltip坐标
+  const [showNodeTooltip, setShowNodeTooltip] = useState(false)
   const [nodeTooltipX, setNodeToolTipX] = useState(0)
-  const [nodeTooltipY, setNodeToolTipY] = useState(-350)
+  const [nodeTooltipY, setNodeToolTipY] = useState(0)
 
   // 节点ContextMenu坐标
-  const [nodeContextMenuX, setNodeContextMenuX] = useState(-300)
+  const [showNodeContextMenu, setShowNodeContextMenu] = useState(false)
+  const [nodeContextMenuX, setNodeContextMenuX] = useState(0)
   const [nodeContextMenuY, setNodeContextMenuY] = useState(0)
   const bindEvents = () => {
     // 监听edge上面mouse事件
@@ -37,27 +40,29 @@ export default function() {
       const point = graph.getCanvasByPoint(x, y)
       setEdgeTooltipX(point.x)
       setEdgeTooltipY(point.y)
+      setShowEdgeTooltip(true)
     })
 
-    graph.on('edge:mouseleave', evt => {
-      setEdgeTooltipX(-300)
+    graph.on('edge:mouseleave', () => {
+      setShowEdgeTooltip(false)
     })
 
     // 监听node上面mouse事件
     graph.on('node:mouseenter', evt => {
-      const { item, target } = evt
+      const { item } = evt
       const model = item.getModel()
       const { x, y } = model
       const point = graph.getCanvasByPoint(x, y)
 
       setNodeToolTipX(point.x - 75)
       setNodeToolTipY(point.y + 15)
+      setShowNodeTooltip(true)
     })
   
     // 节点上面触发mouseleave事件后隐藏tooltip和ContextMenu
     graph.on('node:mouseleave', () => {
-      setNodeToolTipX(-350)
-      setNodeContextMenuX(-300)
+      setShowNodeTooltip(false)
+      setShowNodeContextMenu(false)
     })
 
     // 监听节点上面右键菜单事件
@@ -68,11 +73,13 @@ export default function() {
       const point = graph.getCanvasByPoint(x, y)
       setNodeContextMenuX(point.x)
       setNodeContextMenuY(point.y)
+      setShowNodeContextMenu(true)
     })
   }
 
   useEffect(() => {
     if(!graph) {
+      // TODO 3.1.0版本发布后 layout 部分使用G6内置替代
       const g = new dagre.graphlib.Graph();
       g.setDefaultEdgeLabel(function() {
         return {};
@@ -157,9 +164,9 @@ export default function() {
 
   return (
     <div ref={ref}>
-      <EdgeToolTips x={edgeTooltipX} y={edgeTooltipY} />
-      <NodeTooltips x={nodeTooltipX} y={nodeTooltipY} />
-      <NodeContextMenu x={nodeContextMenuX} y={nodeContextMenuY} />
+      { showEdgeTooltip && <EdgeToolTips x={edgeTooltipX} y={edgeTooltipY} /> }
+      { showNodeTooltip && <NodeTooltips x={nodeTooltipX} y={nodeTooltipY} /> }
+      { showNodeContextMenu && <NodeContextMenu x={nodeContextMenuX} y={nodeContextMenuY} /> }
     </div>
   );
 }
